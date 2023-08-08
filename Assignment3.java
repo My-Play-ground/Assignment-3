99d714ba80e0435ccf3b74ff83949d36653bc8e4
Janaka Daskon <janakadaskon@gmail.com>
2023-08-08 05:54:40 +0530

    Enter your name

--- Assignment3.java
+++ Assignment3.java
@@ -3,8 +3,10 @@ import java.util.Scanner;
 public class Assignment3{
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
         
+        System.out.print("Enter your name: ");
+        String name = scanner.nextLine();
+
     }
 
 
